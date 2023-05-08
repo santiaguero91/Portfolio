283a51a4430a71ead80e5a43874b1548ac675071
Santiago Nicolás Agüero Urquiza <santiagonaguero@gmail.com>
2023-05-08 01:17:16 -0300

    Users y database created

---
+++ app/database.js
@@ -0,0 +1,17 @@
+import cardigan from './img/cardigan.png'
+import countries from "./img/countries.png"
+
+export const Paginas = [
+    {
+        id: 1,
+        name: "CargiganGym",
+        link: "https://cardigangym-santiaguero91.vercel.app",
+        logo: {cardigan}
+    },
+    {
+        id: 1,
+        name: "Henry-Countries",
+        link: "https://www.youtube.com/watch?v=biCLAKvbA3s",
+        logo: {countries}
+    }
+]
