82f3139b8011bb9fa9f0d97edc5d6afd1cdec395
Santiago Nicolás Agüero Urquiza <santiagonaguero@gmail.com>
2023-05-07 23:17:26 -0300

    styledicons

--- app/page.tsx
+++ app/page.tsx
@@ -4,17 +4,17 @@ import { AiOutlineLinkedin,AiOutlineGithub, AiOutlineYoutube } from "react-icons
 /* AiOutlineInstagram */
 export default function Home() {
   return (
-    <div className='flex items-center'>
-    <div className='m-10 bg-transparent text-7xl'>
-      <h2 className='text-cyan-600 font-semibold' data-aos-offset="200">Hi</h2>
+    <div className='flex items-center flex-col'>
+    <div className='mt-10 mb-10 ml-7 mr-64 bg-transparent text-7xl'>
+      <h2 className='text-cyan-600 font-semibold'>Hi</h2>
       <h2 className='text-cyan-600 font-semibold'>I'm Santiago</h2>
       <h3 className='font-semibold'>Full-Stack Developer</h3>
     </div>
-    <div>
-      <ul>
-    <li><AiOutlineLinkedin/> </li>
-    <li><AiOutlineGithub/></li>
-    <li><AiOutlineYoutube/></li>
+    <div className='w-6/12 flex items-center flex-row mr-64'>
+      <ul className=' w-full flex flex-row flex-wrap justify-between items-center'>
+    <li ><AiOutlineLinkedin className='h-12 w-12 cursor-pointer transition duration-300 ease-out hover:text-gray-500'/> </li>
+    <li><AiOutlineGithub className='h-12 w-12 cursor-pointer transition duration-300 ease-out hover:text-gray-500'/></li>
+    <li><AiOutlineYoutube className='h-12 w-12 cursor-pointer transition duration-300 ease-out hover:text-gray-500'/></li>
       </ul>
     </div>
     <div>
