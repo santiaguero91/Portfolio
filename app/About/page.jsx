0c97720306167424cb587c3144236d02f9b51fc7
Santiago Nicolás Agüero Urquiza <santiagonaguero@gmail.com>
2023-05-07 23:52:36 -0300

    contactme button

--- app/About/page.jsx
+++ app/About/page.jsx
@@ -6,10 +6,11 @@ export default function About() {
   return (
 
     <div className='flex flex-row w-3/4 m-auto'>
-      <div className='rounded-md'>
+      <div className='m-auto'>
         <Image src={profilePic} alt='FOTO'
-        width={200}
-        height={200}
+        width={500}
+        height={500}
+        className='rounded-full m-auto'
         />
       </div>
     
@@ -17,7 +18,7 @@ export default function About() {
       <h3 className='my-3'>I’m Santiago Aguero, a Full-Stack Developer located in Buenos Aires managing a serious passion for UI effects, animations and creating intuitive, dynamic user experiences.</h3>
       <h3 className='my-3'>Well-organised person, problem solver, independent employee with attention to detail but also a teamplayer with high communication skills and leadership.</h3>
       <h3 className='my-3'>Interested in the entire programming spectrum and working on ambitious projects with positive people.</h3>
-      <button>Contact Me</button>
+      <button className="mx-auto mt-10 w-fit p-10 bg-cyan-900 hover:bg-cyan-800 text-white font-bold py-2 px-4 rounded"> Contact Me</button>
     </div>
 
     </div>
