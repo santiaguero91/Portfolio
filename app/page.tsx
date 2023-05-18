3365344dc97706da92327377b57e99207222761b
Santiago Nicolás Agüero Urquiza <santiagonaguero@gmail.com>
2023-05-17 21:20:11 -0300

    .

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,73 @@
-import React from 'react'
-import { AiOutlineLinkedin,AiOutlineGithub, AiOutlineYoutube } from "react-icons/ai";
-import Link from "next/link"
+'use client';
+
+import React from "react";
+import {
+  AiOutlineLinkedin,
+  AiOutlineGithub,
+  AiOutlineYoutube,
+} from "react-icons/ai";
+import Link from "next/link";
 import About from "./About/page";
-import {Divider} from "./pageStyle"
+import { Divider, FullStack, Hi } from "./pageStyle";
+import { motion } from "framer-motion";
 
 /* AiOutlineInstagram */
 export default function Home() {
   return (
-    <div className='flex items-center flex-col'>
-    <div className='mt-10 mb-10 ml-7 mr-64 bg-transparent text-7xl'>
-      <h2 className='text-cyan-600 font-semibold'>Hi</h2>
-      <h2 className='text-cyan-600 font-semibold'>I'm Santiago</h2>
-      <h3 className='font-semibold'>Full-Stack Developer</h3>
-    </div>
-    <div className='w-6/12 flex items-center flex-row mr-64'>
-      <div className=' w-full flex flex-row flex-wrap justify-between items-center'>
-    <Link href="https://www.linkedin.com/in/santiago-nicolas-agüero-urquiza-b693a41ba/" ><AiOutlineLinkedin className='h-12 w-12 cursor-pointer transition duration-300 ease-out hover:text-gray-500'/> </Link>
-    <Link href="https://github.com/santiaguero91" ><AiOutlineGithub className='h-12 w-12 cursor-pointer transition duration-300 ease-out hover:text-gray-500'/></Link>
-    <Link href="https://www.youtube.com/@SantiPrograma-mi7qd"><AiOutlineYoutube className='h-12 w-12 cursor-pointer transition duration-300 ease-out hover:text-gray-500'/></Link>
+    <div className="flex items-center flex-col">
+      <div className="mt-10 mb-10 ml-7 mr-64 bg-transparent text-7xl">
+        <Hi
+          className="text-cyan-600 font-semibold"
+          initial={{ x: -1000 }}
+          animate={{ x: 0, transition: { duration: 0.9 } }}
+        >
+          Hi
+        </Hi>
+        <Hi
+          className="text-cyan-600 font-semibold"
+          initial={{ x: -1000 }}
+          animate={{ x: 0, transition: { duration: 0.8, delay: 0.9 } }}
+        >
+          I'm Santiago
+        </Hi>
+        <FullStack className="font-semibold" 
+        initial={{scaleY:0}}
+        animate={{scaleY:1}}
+        transition={{ delay: 1.8 , transition:{duration:0.5}}}
+        >Full-Stack Developer</FullStack>
+      </div>
+      <div className="w-6/12 flex items-center flex-row mr-64">
+        <div className=" w-full flex flex-row flex-wrap justify-between items-center">
+          <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 , transition: { duration: 0.8, delay: 2 }}}
+          exit={{ opacity: 0, transition: { duration: 0.2 } }}>
+          <Link href="https://www.linkedin.com/in/santiago-nicolas-agüero-urquiza-b693a41ba/">
+            <AiOutlineLinkedin className="h-12 w-12 cursor-pointer transition duration-300 ease-out hover:text-gray-500" />{" "}
+          </Link></motion.div>
+          <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 , transition: { duration: 0.8, delay: 2.2 }}}
+          exit={{ opacity: 0, transition: { duration: 0.2 } }}>
+          <Link href="https://github.com/santiaguero91">
+            <AiOutlineGithub className="h-12 w-12 cursor-pointer transition duration-300 ease-out hover:text-gray-500" />
+          </Link></motion.div>
+          <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 , transition: { duration: 0.8, delay: 2.5 }}}
+          exit={{ opacity: 0, transition: { duration: 0.2 } }}>
+          <Link href="https://www.youtube.com/@SantiPrograma-mi7qd">
+            <AiOutlineYoutube className="h-12 w-12 cursor-pointer transition duration-300 ease-out hover:text-gray-500" />
+          </Link> </motion.div>
+        </div>
+      </div>
+      <Divider />
+      <div>
+        <About />
+        <Divider />
+
+        <button></button>
       </div>
     </div>
-    <Divider/>
-    <div>
-      <About/>
-    <Divider/>
-
-    <button></button>
-    </div>
-    </div>
-  )
-
+  );
 }
--- app/pageStyle.js
+++ app/pageStyle.js
@@ -1,12 +1,20 @@
 'use client';
+import { motion } from "framer-motion";
 
 import styled from 'styled-components';
 
-export const Divider = styled.div`
+export const Divider = styled(motion.div)`
 margin: 10rem 0 5rem;
 background-color: rgb(98,98,98);
 width: 100%;
 height:  5rem;
 border: none;
 box-shadow: rgba(250, 250, 193, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
+`;
+
+
+export const Hi = styled(motion.h2)`
+`;
+
+export const FullStack = styled(motion.h3)`
 `;
